8a6f6841f4b06eb395161751ab7f445720264a14
Matildha <tildhanor@gmail.com>
2017-06-24 17:05:42 +0200

    Added temp and library folders to gitignore. Added vertical limits to player controller.

--- Tatari/Assets/PlayerController.cs
+++ Tatari/Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
     public float speed = 2.5f;
     public float mouseSensitivity = 2.0f;
     GameObject playerCamera;
+    float rotationX = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,11 +21,14 @@ public class PlayerController : MonoBehaviour {
         float moveFB = Input.GetAxis("Vertical") * speed;  // Front, back movement
 
         float rotationY = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float rotationX = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotationX = Mathf.Clamp(rotationX + Input.GetAxis("Mouse Y") * mouseSensitivity, -20, 20);
+        //playerCamera.gameObject.transform.localEulerAngles.Set(Mathf.Clamp(rotationX, -60, 60), 0, 0);
+        //playerCamera.gameObject.transform.localEulerAngles.Set(-rotationX, 0, 0);
+        playerCamera.gameObject.transform.localEulerAngles = new Vector3(-rotationX, 0, 0);
 
         transform.Translate(moveLR * Time.deltaTime, 0, moveFB * Time.deltaTime);
         transform.Rotate(0, rotationY, 0);
-        playerCamera.gameObject.transform.Rotate(-rotationX, 0, 0);
+        //playerCamera.gameObject.transform.Rotate(-rotationX, 0, 0);
 
         if(Input.GetKeyDown("escape"))
         {
