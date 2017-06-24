c9ae8e4cfcaa64fc92b3b5177d5b41e2eaa70c8f
Matildha <tildhanor@gmail.com>
2017-06-24 19:55:49 +0200

    Fixed milestone 5 - turn lantern on/off. With both light and emission glow.

---
+++ Tatari/Assets/Scripts/Lantern.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lantern : MonoBehaviour {
+
+    GameObject lanternLight;
+    bool lightToggle = true;
+    Material lanternGlow;
+    Color defaultColor;
+
+    private void Start()
+    {
+        lanternLight = this.transform.Find("Lantern Light").gameObject;
+        lanternGlow = GetComponent<Renderer>().material;
+        defaultColor = lanternGlow.GetColor("_EmissionColor");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown("space"))
+        {
+            lanternLight.SetActive(!lightToggle);
+            lanternGlow.EnableKeyword("_EMISSION");
+            if (lightToggle)
+                lanternGlow.SetColor("_EmissionColor", Color.black);
+            else
+                lanternGlow.SetColor("_EmissionColor", defaultColor);
+            lightToggle ^= true; // xor
+            print("Pressed space");
+        }
+		
+	}
+}
