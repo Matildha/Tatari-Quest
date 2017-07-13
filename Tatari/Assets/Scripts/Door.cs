90c97d4d8d5f1cc710854884b85e7a583b3f8035
Matildha <tildhanor@gmail.com>
2017-07-13 13:19:12 +0200

    Started building real house and changed player controller. Commented old code remains.

--- Tatari/Assets/Scripts/Door.cs
+++ Tatari/Assets/Scripts/Door.cs
@@ -3,22 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : Interactable{
-
-    //public int worldAreaID;
-
+public class Door : Interactable
+{
     public float speed = 1f;
-    //Set with Unity interface  
-    public Vector3[] positions = { new Vector3(-4.52f, 2.13f, -6.82f),             
-                                    new Vector3(-4.58f, 2.13f, -3.95f) };  // Two possible positions for the door (SIZE 2)
-
-    const string PROMPT_MSG = "Press E to move door";  
-    int targetPosition;
+    public Vector3 motion;
+    Vector3 startPos;
+    int targetPosition;  // Will be 1 if door is on startPos and -1 if door is on startPos + motion
     bool isMoving;
 
+    const string PROMPT_MSG = "Press E to move door";
+    
+   
     private void Start()
     {
         targetPosition = 1;
+        startPos = transform.localPosition;
     }
 
     public override string PromptMessage { get { return PROMPT_MSG; } }
@@ -32,13 +31,14 @@ public class Door : Interactable{
     {
         if (isMoving)
         {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition,
-                                positions[targetPosition], speed * Time.deltaTime);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, 
+                                                            startPos + (motion * targetPosition),
+                                                            speed * Time.deltaTime);
         }
-        if (transform.localPosition == positions[targetPosition])
+        if (transform.localPosition == startPos + (motion * targetPosition))
         {
             isMoving = false;
-            targetPosition = (++targetPosition) % 2;
+            targetPosition *= -1;
         }
     }
 }
---
+++ Tatari/Assets/Scripts/Door_Old.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Old : Interactable{
+
+    //public int worldAreaID;
+
+    public float speed = 1f;
+    //Set with Unity interface  
+    public Vector3[] positions = { new Vector3(-4.52f, 2.13f, -6.82f),             
+                                    new Vector3(-4.58f, 2.13f, -3.95f) };  // Two possible positions for the door (SIZE 2)
+
+    const string PROMPT_MSG = "Press E to move door";  
+    int targetPosition;
+    bool isMoving;
+
+    private void Start()
+    {
+        targetPosition = 1;
+    }
+
+    public override string PromptMessage { get { return PROMPT_MSG; } }
+
+    public override void Interact()
+    {
+        isMoving = true;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition,
+                                positions[targetPosition], speed * Time.deltaTime);
+        }
+        if (transform.localPosition == positions[targetPosition])
+        {
+            isMoving = false;
+            targetPosition = (++targetPosition) % 2;
+        }
+    }
+}
